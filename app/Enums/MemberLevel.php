1d00f003ebac7209139ac2e6715b055fb0ca5a42
Congkaka <trinhvancong1996@gmail.com>
2024-12-04 15:12:15 +0700

    update db seed permission

--- app/Enums/MemberLevel.php
+++ app/Enums/MemberLevel.php
@@ -6,12 +6,14 @@ enum MemberLevel: string
 {
     case ADMIN = 'ADMIN';
     case WRITER = 'WRITER';
+    case STAFF = 'STAFF';
 
     public static function getMap(): array
     {
         return [
             self::ADMIN->value => 'Admin',
-            self::WRITER->value => 'Writer'
+            self::WRITER->value => 'Writer',
+            self::STAFF->value => 'STAFF'
         ];
     }
 }
