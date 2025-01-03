758dff4a20cfbbd173a54445060b229013515293
congkaka <trinhvancong1996@gmail.com>
2025-01-03 17:27:00 +0700

    update orderby effectiveDate api getRegulation

--- app/Http/Controllers/Api/RegulationController.php
+++ app/Http/Controllers/Api/RegulationController.php
@@ -19,7 +19,7 @@ class RegulationController extends Controller
         if(!empty($request->validity_type)) $regulations->where('validity_type', '=', $request->validity_type);
 
         $regulations = $regulations
-        ->orderBy('created_at', 'DESC')
+        ->orderBy('effectiveDate', 'DESC')
         ->paginate(10);
         // foreach($regulations as $regulation) {
         //     $regulation->download_path = $regulation->download_path;
